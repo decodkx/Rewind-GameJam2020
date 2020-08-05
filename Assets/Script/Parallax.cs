0bf5e1e9d9dd63b209933862aae865d7d9811f68
decodkx <43831556+decodkx@users.noreply.github.com>
2020-08-04 22:53:57 -0300

    1.7
    Parallax
    timer
    ajuste de movimentação

---
+++ Assets/Script/Parallax.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Parallax : MonoBehaviour
+{
+    private float startpos, length, startposy;
+    public Camera cam;
+    public float parallaxEffect;
+
+    void Start()
+    {
+        startpos = transform.position.x;
+        startposy = transform.position.y;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+    
+    void Update()
+    {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float dist = (cam.transform.position.x * parallaxEffect);
+
+        float distt = (cam.transform.position.y * parallaxEffect);
+
+        transform.position = new Vector2 (startpos + dist, startposy + distt);
+
+        if (temp > startpos + length) 
+            startpos += length;
+        else if (temp < startpos - length) 
+            startpos -= length;
+    }
+}
---
+++ Assets/Script/Timer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timer;
+    GameController gameController;
+
+    float startTime = 60;
+    float t;
+    string minutes;
+    string seconds;
+
+    bool ended = false;
+    void Start()
+    {
+        gameController = FindObjectOfType<GameController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!ended)
+        {
+            t = startTime - Time.time;
+            minutes = ((int)t / 60).ToString();
+            seconds = (t % 60).ToString("f3");
+
+            timer.text = minutes + ":" + seconds;
+        }
+
+        if (t < 0.01 && !ended)
+        {
+            timer.text = "0:00,000";
+            ended = true;
+            gameController.setGameOver();
+        }
+
+
+    }
+}
