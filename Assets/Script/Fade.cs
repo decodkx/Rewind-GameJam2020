3da51f412b890bf13c77eab099e1700caefaa227
decodkx <43831556+decodkx@users.noreply.github.com>
2020-08-05 22:01:37 -0300

    2.5 a try salvage
    backup

---
+++ Assets/Script/Fade.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Fade : MonoBehaviour
+{
+
+
+    public GameObject painelFume;
+    public Image fume;
+    public Color[] corTransicao;
+    public float step;
+
+
+    private void Start()
+    {
+        StartCoroutine("FadeO");
+    }
+    public void FadeIn()
+    {
+        painelFume.SetActive(true);
+        StartCoroutine("FadeO");
+    }
+
+    public void FadeOut()
+    {
+  
+        StartCoroutine("FadeO");
+     
+    }
+
+    IEnumerator FadeI()
+    {
+
+        for (float i = 0; i <= 1; i += step)
+        {
+            fume.color = Color.Lerp(corTransicao[0], corTransicao[1], i); //Muda a cor 
+            yield return new WaitForEndOfFrame();
+
+        }
+
+    }
+
+
+    IEnumerator FadeO()
+    {
+        yield return new WaitForSeconds(0.9F);
+
+
+        for (float i = 0; i <= 1; i += step)
+        {
+
+            fume.color = Color.Lerp(corTransicao[1], corTransicao[0], i); //Muda a cor 
+            yield return new WaitForEndOfFrame();
+
+        }
+        painelFume.SetActive(false);
+    }
+
+
+
+}
+
