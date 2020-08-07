231e2f6edf464fb1849fc08b982400a2800771b7
decodkx <43831556+decodkx@users.noreply.github.com>
2020-08-07 16:21:58 -0300

    4.4
    musica

---
+++ Assets/Script/Music.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayAudio(AudioClip audioClip)
+    {
+        audioSource.pitch = 1;
+        audioSource.clip = audioClip;
+        audioSource.Play();
+
+    }
+    public void PlayReverseAudio(AudioClip audioClip)
+    {
+        audioSource.pitch = -1;
+        audioSource.loop = true;
+        audioSource.clip = audioClip;
+        audioSource.Play();
+        StartCoroutine(StopLoop());
+    }
+
+    public IEnumerator StopLoop()
+    {
+        yield return new WaitForSeconds(1f);
+        audioSource.loop = false;
+    }
+
+}
