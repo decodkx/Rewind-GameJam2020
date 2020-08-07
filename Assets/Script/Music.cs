518887758e43db888a98256029b7ce9acf94e694
decodkx <43831556+decodkx@users.noreply.github.com>
2020-08-07 17:29:34 -0300

    4.6
    musica e obstaculos

--- Assets/Script/Music.cs
+++ Assets/Script/Music.cs
@@ -5,32 +5,64 @@ using UnityEngine;
 public class Music : MonoBehaviour
 {
     private AudioSource audioSource;
+    public AudioClip audioClip;
+    public Move tape;
+
+    int state; 
+    int tempState = 1;
+    bool change = false;
 
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        audioSource.loop = true;
+        PlayAudio();
     }
 
-    public void PlayAudio(AudioClip audioClip)
+    void Update()
+    {
+        state = tape.inversor;
+        if (tempState != state)
+        {
+            change = true;
+        }
+        else change = false;
+        tempState = state;
+
+        if(change)
+        {
+            if (tape.inversor == -1)
+                PlayReverseAudio();
+        }
+
+        if (change)
+        {
+            if (tape.inversor == 1)
+                PlayAudio();
+        }
+    }
+
+    public void PlayAudio()
     {
         audioSource.pitch = 1;
+        audioSource.volume = 0.35f;
         audioSource.clip = audioClip;
         audioSource.Play();
 
     }
-    public void PlayReverseAudio(AudioClip audioClip)
+    public void PlayReverseAudio()
     {
         audioSource.pitch = -1;
-        audioSource.loop = true;
+        audioSource.volume = 0.55f;
         audioSource.clip = audioClip;
         audioSource.Play();
-        StartCoroutine(StopLoop());
+        //StartCoroutine(StopLoop());
     }
 
-    public IEnumerator StopLoop()
-    {
-        yield return new WaitForSeconds(1f);
-        audioSource.loop = false;
-    }
+    //public IEnumerator StopLoop()
+    //{
+    //    yield return new WaitForSeconds(0.5f);
+    //    audioSource.loop = false;
+    //}
 
 }
